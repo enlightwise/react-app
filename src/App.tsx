c310b4c9f0c606b49951f41ed86e309cb7f2b621
enlightwise <enlightwise@protonmail.com>
2024-12-31 16:24:33 +0100

    showing alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
         Button
       </Button>
     </div>
